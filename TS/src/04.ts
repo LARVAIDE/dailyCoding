cc4d93c8fff158a8ad9cbde90152cb63de7dbd20
larvide <larvide@91zhen.com>
2022-07-07 18:35:22 +0800

    feat: save

--- TS/src/04.ts
+++ TS/src/04.ts
@@ -7,3 +7,17 @@ const foo: object = function(){
 const obj: {foo: number} = {
     foo: 1111
 }
+
+
+const sum: (a: number, b: number) => number = (a, b) => {
+    return a + b;
+}
+
+interface sum_ {
+    (a: number, b: number): number
+}
+const sum_: sum_ = (a, b) => {
+    return a + b;
+}
+
+sum_(1, 0);
--- TS/src/16.ts
+++ TS/src/16.ts
@@ -1,7 +1,16 @@
 // 泛型
 //对于不能确定类型，在调用时作为参数传递进去
-
 export {}
+
+function getId<T> (value: T): T {
+    return value;
+}
+
+// const sum: (a: number, b: number) => number = (a, b) => {
+//     return a + b;
+// }
+
+const GetId<T>: (value: T) => T = (value) => value
 
 function createArr<T>(length: number, value: T): Array<T>{
     return Array<T>(length).fill(value)
