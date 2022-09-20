18074746321f9adbce03bb32409590b52c72443f
larvide <larvide@91zhen.com>
2022-09-20 12:18:53 +0800

    save

---
+++ ES6_API/EventEmitter/index.js
@@ -0,0 +1,64 @@
+class MyEventEmitter { 
+    static instance = null;
+    static getInstance() {
+        if (!MyEventEmitter.instance) {
+            MyEventEmitter.instance = new MyEventEmitter()
+        }
+        return MyEventEmitter.instance
+    }
+
+    constructor() { 
+        this.events = {}
+    }
+
+    /**
+     * 订阅
+     * @param {*} name 
+     * @param {*} event 
+     */
+    on(name, event) { 
+        const eventList = this.events[name] || []
+        this.events[name] = eventList.concat(event)
+    }
+
+    /**
+     * 执行
+     * @param {*} name 
+     */
+    emit(name, ...args) { 
+        const eventList = this.events[name] || []
+        eventList.forEach(item => item.apply(item, args));
+    }
+
+    /**
+     * 取消
+     * @param {*} name 
+     * @param {*} event 
+     */
+    off(name, event) { 
+        if(this.events[name]) { 
+            this.events[name] = this.events[name].filter(item => item !== event)
+        }
+    }
+
+    /** */
+    once(name, ) { 
+
+    }
+}
+
+class EventEmitter { 
+    constructor() { 
+        return MyEventEmitter.getInstance()
+    }
+}
+
+
+const eventBus = new EventEmitter()
+const eventBus1 = new EventEmitter()
+console.log(eventBus === eventBus1) // 打印输出: true
+function handleClick(param1, param2) {
+  console.log(param1, param2)
+}
+eventBus.on('click', handleClick)
+eventBus.emit('click', 'foo', 'bar') // 打印输出: foo bar
