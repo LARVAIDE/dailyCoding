772c336906ffbee8600509d78e9af6817af0e735
larvide <1337781978@qq.com>
2023-02-12 16:37:59 +0800

    feat(update vite config): suport to image and so on

--- packages/vite-project/src/components/Header/index.tsx
+++ packages/vite-project/src/components/Header/index.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import { devDependencies } from '../../../package.json';
+import logoSrc from '@assets/imgs/bkg.jpg';
 import './index.module.scss';
 
+import Worker from './example.js?worker';
+// 1. 初始化 Worker 实例
+const worker = new Worker();
+// 2. 主线程监听 worker 的信息
+worker.addEventListener('message', (e) => {
+  console.log(e);
+});
+
 export function Header() {
+  useEffect(() => {
+    const img = document.getElementById('logo') as HTMLImageElement;
+    img.src = logoSrc;
+  }, []);
   return (
     <div className="p-20px text-center header">
       <h1 className="font-bold text-2xl mb-2">
@@ -16,6 +30,8 @@ export function Header() {
       >
         Button
       </button>
+
+      <img id="logo" className="m-auto mb-4" alt="" />
     </div>
   );
 }
--- packages/vite-project/vite.config.ts
+++ packages/vite-project/vite.config.ts
@@ -5,11 +5,28 @@ import autoprefixer from 'autoprefixer';
 import windi from 'vite-plugin-windicss';
 import viteEslint from 'vite-plugin-eslint';
 import viteStylelint from 'vite-plugin-stylelint';
+import svgr from 'vite-plugin-svgr';
+import viteImagemin from 'vite-plugin-imagemin';
 
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
+const isProduction = process.env.NODE_ENV === 'production';
+const CDN_URL = 'xxxxxx';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProduction ? CDN_URL : '/',
+  resolve: {
+    alias: {
+      '@assets': path.join(__dirname, 'src/assets')
+    }
+  },
+  optimizeDeps: {
+    entries: ['./src/App.tsc']
+  },
+  build: {
+    assetsInlineLimit: 8 * 1024
+  },
   css: {
     modules: {
       // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
@@ -19,7 +36,6 @@ export default defineConfig({
     postcss: {
       plugins: [
         autoprefixer({
-          // 指定目标浏览器
           overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
         })
       ]
@@ -36,6 +52,29 @@ export default defineConfig({
     viteEslint(),
     viteStylelint({
       exclude: /windicss|node_modules/
+    }),
+    svgr(),
+    viteImagemin({
+      // 无损压缩配置，无损压缩下图片质量不会变差
+      optipng: {
+        optimizationLevel: 7
+      },
+      // 有损压缩配置，有损压缩下图片质量可能会变差
+      pngquant: {
+        quality: [0.8, 0.9]
+      },
+      // svg 优化
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
     })
   ]
 });
