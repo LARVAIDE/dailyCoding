2cad998239447a872b287b86625a52c14a61c606
larvide <1337781978@qq.com>
2023-04-25 00:56:45 +0800

    feat: debug antd

---
+++ packages/debugSourceCode/antd-react-test/src/App.js
@@ -0,0 +1,18 @@
+import { Button } from 'antd';
+import 'antd/dist/antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button
+        type="primary"
+        size="large"
+      >
+        sss
+      </Button>
+    </div>
+  );
+}
+
+export default App;
